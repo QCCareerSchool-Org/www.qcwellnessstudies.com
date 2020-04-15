cb61684ef863ac3a46db8e14cfa6d2e01784fde4
Dave Welsh <dave@qccareerschool.com>
2020-04-15 14:55:46 -0400

    Updates footer links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,12 @@
+import Link from 'next/link';
 import React from 'react';
 
 import { Address } from './address';
 import { TelephoneNumber } from './telephone-number';
+
+declare global {
+  interface Window { LC_API: { open_chat_window: () => void } }
+}
 
 export const Footer: React.FC = () => (
   <footer id="footer" className="footer bg-dark mt-auto">
@@ -12,37 +17,37 @@ export const Footer: React.FC = () => (
         </div>
         <div className="col-12 col-md-4 col-lg-3 mb-4 mb-md-0">
           <p className="text-light"><strong>AVAILABLE CERTIFICATIONS:</strong></p>
-          <p><a href="/courses-and-tuition/personal-care-aide/">Personal Care Aide</a></p>
-          <p><a href="/courses-and-tuition/sleep-consultant/"> Sleep Consultant</a></p>
-          <p><a href="/courses-and-tuition/skincare-consultant/">Skincare Consultant</a></p>
-          <p><a href="/courses-and-tuition/professional-organizing/">Professional Organizer</a></p>
+          <p><Link href="/courses-and-tuition/personal-care-aide"><a>Personal Care Aide</a></Link></p>
+          <p><Link href="/courses-and-tuition/sleep-consultant"><a> Sleep Consultant</a></Link></p>
+          <p><Link href="/courses-and-tuition/skincare-consultant"><a>Skincare Consultant</a></Link></p>
+          <p><Link href="/courses-and-tuition/aging-in-place"><a>Aging in Place</a></Link></p>
         </div>
         <div className="col-12 col-md-4 col-lg-3 mb-4 mb-md-0">
           <p className="text-light"><strong>GETTING STARTED:</strong></p>
-          <p><a href="/faq/">Frequently Asked Questions</a></p>
-          <p><a href="/courses-and-tuition/">Tuition</a></p>
-          <p><a href="/about/#guarantee">Money-Back Guarantee</a></p>
+          <p><Link href="/faq"><a>Frequently Asked Questions</a></Link></p>
+          <p><Link href="/courses-and-tuition"><a>Tuition</a></Link></p>
+          <p><Link href="/about/#guarantee"><a>Money-Back Guarantee</a></Link></p>
           <p className="text-secondary"><strong><a href="https://enroll.qcwellnessstudies.com/">ENROLL NOW</a></strong></p>
         </div>
         <div className="col-12 col-md-4 col-lg-3 mb-4 mb-md-0">
           <p className="text-light"><strong>CONTACT US:</strong></p>
           <p><a href="mailto:info@qcwellnessstudies.com">Send Us a Message</a></p>
-          <p><a href="#" onClick={() => { (window as any).LC_API?.open_chat_window(); return false; }}>Click Here to Chat</a></p>
+          <p><a href="#" onClick={(): false => { window.LC_API?.open_chat_window(); return false; }}>Click Here to Chat</a></p>
           <p><TelephoneNumber /></p>
-          <p><a target="_blank" href="/terms/" className="termsLink">Privacy Policy</a></p>
+          <p><a target="_blank" href="/terms" className="termsLink">Privacy Policy</a></p>
         </div>
       </div>
       <hr />
       © {(new Date()).getFullYear()} QC Wellness Studies.<br />
       <span id="footerAddress"><Address /></span>
       <p className="d-none d-md-block">
-        <a target="_blank" href="https://www.qcmakeupacademy.com/">QC Makeup Academy</a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.qcmakeupacademy.com/">QC Makeup Academy</a>
         <span className="px-2">|</span>
-        <a target="_blank" href="https://www.qceventplanning.com/">QC Event School</a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.qceventplanning.com/">QC Event School</a>
         <span className="px-2">|</span>
-        <a target="_blank" href="https://www.qcdesignschool.com/">QC Design School</a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.qcdesignschool.com/">QC Design School</a>
         <span className="px-2">|</span>
-        <a target="_blank" href="https://www.doggroomingcourse.com/">QC Pet Studies</a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.doggroomingcourse.com/">QC Pet Studies</a>
       </p>
     </div>
   </footer>
