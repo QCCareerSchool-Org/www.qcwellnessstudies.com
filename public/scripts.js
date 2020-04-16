5fd2ed5f8baf00986ef3ef74f44c307cad55bd26
Dave Welsh <dave@qccareerschool.com>
2020-04-16 12:19:10 -0400

    Removes adroll

--- public/scripts.js
+++ public/scripts.js
@@ -1,20 +1,22 @@
-// Adroll
-adroll_adv_id = "BRIHHIIWTBCJ7P3X5A3V3Y";
-adroll_pix_id = "AAKTSZEP5NA3PNKFUT5BJB";
-(function () {
-  var _onload = function () {
-    if (document.readyState && !/loaded|complete/.test(document.readyState)) { setTimeout(_onload, 10); return }
-    if (!window.__adroll_loaded) { __adroll_loaded = true; setTimeout(_onload, 50); return }
-    var scr = document.createElement("script");
-    var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
-    scr.setAttribute('async', 'true');
-    scr.type = "text/javascript";
-    scr.src = host + "/j/roundtrip.js";
-    ((document.getElementsByTagName('head') || [null])[0] || document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
-  };
-  if (window.addEventListener) { window.addEventListener('load', _onload, false); }
-  else { window.attachEvent('onload', _onload) }
-}());
+/* eslint-disable */
+
+// // Adroll
+// adroll_adv_id = "BRIHHIIWTBCJ7P3X5A3V3Y";
+// adroll_pix_id = "AAKTSZEP5NA3PNKFUT5BJB";
+// (function () {
+//   var _onload = function () {
+//     if (document.readyState && !/loaded|complete/.test(document.readyState)) { setTimeout(_onload, 10); return }
+//     if (!window.__adroll_loaded) { __adroll_loaded = true; setTimeout(_onload, 50); return }
+//     var scr = document.createElement("script");
+//     var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
+//     scr.setAttribute('async', 'true');
+//     scr.type = "text/javascript";
+//     scr.src = host + "/j/roundtrip.js";
+//     ((document.getElementsByTagName('head') || [null])[0] || document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
+//   };
+//   if (window.addEventListener) { window.addEventListener('load', _onload, false); }
+//   else { window.attachEvent('onload', _onload) }
+// }());
 
 // ActiveCampaign
 if (!window.fetch) {
