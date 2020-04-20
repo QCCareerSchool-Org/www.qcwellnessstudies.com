30ee37373f9adf2c0de08a6e296d60c9b70be44f
Alex Monteiro <alexjmcm@Alexs-MacBook-Pro.local>
2020-04-20 16:18:46 -0400

    Adds student-feature page

---
+++ src/pages/student-feature.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+
+import { DefaultLayout } from '../layouts/default';
+
+const Page: React.FC = () => (
+  <DefaultLayout>
+
+    <section id="heroSection">
+      <div className="container">
+        <div className="row d-flex align-items-center">
+          <div className="col-12 mb-4 col-sm-10 col-md-8 col-lg-12 mb-lg-0">
+            <h1 className="text-dark">Graduate Feature: Sarah Pashniak</h1>
+            <p className="lead">Ordered List</p>
+          </div>
+          <div className="col-12 mb-4 col-sm-10 col-md-8 col-lg-6 mb-lg-0">
+            <h3>Tell us a little bit about yourself!</h3>
+            <p>My name is Sarah Pashniak, and I am passionate about sleep! (Hence the fun little pun in my business name!)  I am the mama of three children: twin boys (age 4) and a 1-year-old daughter. I’ve also been a kindergarten teacher for the past 7 years. When I was in University to complete my education degree, I majored in Psychology. I’m fascinated by human behavior and how the mind works! I love my little family, and feel like I truly enjoy my children because we’re always feeling well-rested and happy. This is a huge reason why I’m so passionate about sleep!</p>
+            <h3>Why did you decide to become a certified Infant/Pediatric Sleep Consultant? </h3>
+            <p>This is a loaded question, because there are SO many reasons why I took the plunge and decided to become a Sleep Coach. After the birth of our twin boys, I suffered with postpartum depression and anxiety. Having twins is wonderful in so many ways, but it was also a major life adjustment. I felt like my world was turned upside down; I had difficulty coping. The lack of sleep made my anxiety and depression worse. My mind was filled with spiraling thoughts and “what ifs”. When one baby was awake, the other was sleeping, and vice-versa. I barely slept for 4 months! I knew I needed something to change. So I read books and spent hours (in the middle of the night), learning about sleep. I used this knowledge to sleep train the boys. After that, things started looking up! The boys started sleeping regularly, and at the same time – which meant that I could as well. Slowly but surely, I felt the fog lift. I began to feel more and more like myself, and was able to better manage my emotions. I could also finally receive help for my PPA / PPD. Sleep had such an incredible impact on my mental health! I’d suffered for a few months with my postpartum depression and anxiety, but I know some women struggle for much longer. I wanted to help other parents who were struggling with a lack of sleep. After that, when my daughter was 4 months old, I actually worked with a Sleep Consultant for her. It was this individual who inspired me to get my certification as well! We became instant best friends. Along the way, she’s also been my mentor. She recognized my passion and desire to not only help babies, but to help the whole family get the sleep they need and deserve. </p>
+          </div>
+          <div className="col-12 col-sm-10 col-md-8 col-lg-6">
+            <p><img className="img-fluid" src={require('../images/student-feature/sarah-pashniak-family.jpg')} alt="Sarah Pashniak Family" /></p>
+          </div>
+          <div className="col-12 col-sm-10 col-md-8 col-lg-6">
+            <p><img className="img-fluid" src={require('../images/student-feature/sarah-pashniak-family-lake.jpg')} alt="Sarah Pashniak Family in the Lake" /></p>
+          </div>
+          <div className="col-12 mb-4 col-sm-10 col-md-8 col-lg-6 mb-lg-0">
+            <h3>Tell us about your Sleep Consulting business, Pashionate About Sleep! How has the experience of starting and running your own successful business been for you?</h3>
+            <p>Starting and then running my own business has been very challenging – yet extremely rewarding. This time last year, I never would have imagined that I’d have a successful business that’s all mine! Sleep, and the benefits of sleep, has always been an interest of mine. But I was always afraid of taking the plunge. I was hesitant about the “what ifs”! I am SO glad that I took a leap of faith and pursued my goals, because I’m extremely proud of my business. I take great pride in the work I’ve done, and the parents I’ve been able to help and get to know along the way. I’m truly passionate about what I do. That, in large part, has fueled my motivation to be persistent! That being said, there was a lot of work involved. (I cannot stress this enough!) But if you are truly dedicated to helping your clients get the rest they need, and improving their mental health, then all of the hard work is worth it. The most rewarding part of my job is receiving messages of gratitude from clients who were once sleep-deprived and exhausted. Helping parents get their babies to sleep is only one part of this job. The ripple effects that come AFTER baby is sleeping trained is what I love the most about what I do.</p>
+            <h3>Having graduated from QC’s Sleep Consultant Course, how would you say that your training with QC helped prepare you for the professional world of being a Pediatric Sleep Coach? </h3>
+            <p> The Sleep Consultant Course divides the learning into easy-to-digest portions. The assignments effectively reflect the information taught in each section, and there’s a variety of different types of assignments to test and apply your knowledge. Quick assessments throughout the program allow you to regularly check in on your progress, and keep track of where your strengths and weaknesses are. There are also smaller assignments as well, which gradually build towards one larger, final assignment at the end of every Unit. I really enjoyed that I received thorough feedback after each assignment from my tutor. I was able to learn from it and refer back to it as often as needed, in order to improve my work and grade. I also found that the course includes sample materials that you can adapt and make your own. By the time you complete the course, you’ll have so many materials that you’ll be able to use in your actual career, such as</p>
+            <ul>
+              <li>Intake forms</li>
+              <li>Sleep plans</li>
+              <li>A business logo</li>
+              <li>And so much more! </li>
+            </ul>
+          </div>
+          <div className="col-12 mb-4 col-sm-10 col-md-8 col-lg-6 mb-lg-0">
+            <h3>What was your experience like as a QC student, taking an online course? </h3>
+            <p>I had a WONDERFUL experience as a student at QC Wellness School! My favorite thing about my program was the quick feedback from my instructor. I enjoyed that it was always provided to me in a detailed audio message. It felt so personal! I also found the turnaround rate (from when I’d submit my Unit to when I’d receive my tutor’s feedback) to be exceptionally fast. If I needed any support or had questions about my assignment, I could ask my tutor. They were always there to help! Not to mention, there was also the Student Support Team. I was always able to reach out to them by phone, email, or live chat. The entire QC staff supports you, helps you with assignments, and answers any questions you may have!</p>
+            <h3>You completed the optional Business Unit at the end of your course. Not everyone takes advantage of this additional business training! What did it teach you, and would you recommend it for others taking the Sleep Consultant Course?</h3>
+            <p>I would definitely recommend that you take the additional business training, especially if you have any goals of starting your own business. After all, how you market your business is key to your success! If you have no business training, how can you ever hope to run one?</p>
+          </div>
+          <div className="col-12 col-sm-10 col-md-8 col-lg-6">
+            <p><img className="img-fluid" src={require('../images/student-feature/sarah-pashniak-living-room.jpg')} alt="Sarah Pashniak Living Room" /></p>
+          </div>
+          <div className="col-12 col-sm-10 col-md-8 col-lg-6">
+            <p><img className="img-fluid" src={require('../images/student-feature/sarah-pashniak-office.jpg')} alt="Sarah Pashniak Office" /></p>
+          </div>
+          <div className="col-12 mb-4 col-sm-10 col-md-8 col-lg-6 mb-lg-0">
+            <h3>What do you find is the most challenging part when guiding and helping clients to sleep train their infants? </h3>
+            <p>Letting the client know that it’s okay to ask for help. A lot of the time, parents think that they’re doing something wrong if their baby doesn’t sleep well. They feel that they should be able to figure it out all by themselves, and that if they can’t, they’re somehow falling short as a parent. But this is not true at all! Also, I often hear mothers say that they were always under the impression that constantly being tired was “just a part” of motherhood. This is also false! As a professional Sleep Consultant, it’s important to remind parents that they’re doing a fabulous job, and that it’s not only okay, but healthy to ask for support if they need it.</p>
+            <h3>Your online presence has a very prominent and beautiful aesthetic - especially your Instagram page. How would you describe your brand, and how do you feel this aesthetic translates to the brand? </h3>
+            <p>To be honest, social media is a mystery to me! I’m slowly learning more about it, though. I was never overly into sites like Facebook or Instagram before. But in today’s market, these types of online forums are essential for a successful business! Your website and social media pages reflect your business. You need to find a balance of demonstrating knowledge and promoting yourself and your brand. Your first impression on both potential clients and followers is made by the overall look of your page. As a general tip: when you begin building your business on these platforms, consider the colors and content you want to be associated with your business. This will help build your brand and make you recognizable to your audience!</p>
+          </div>
+          <div className="col-12 mb-4 col-sm-10 col-md-8 col-lg-12 mb-lg-0">
+            <h3>Why did you decide to attend an online school to earn your Sleep Consultant certification?</h3>
+            <p>I chose an online school because I have three children at home and I needed flexibility.  I liked that I was able to dedicate chunks of time whenever I had a spare hour or two. I chose QC Wellness Studies to complete my certification because it offered a number of different perks that worked for my budget and busy lifestyle. In particular</p>
+            <ol>
+              <li>I found the course to be very affordable.</li>
+              <li>I liked how my questions were responded to quickly by the staff, and always in a friendly manner.</li>
+              <li>I was able to complete the course at my OWN pace. This is a great option if you have children, another job, etc.</li>
+              <li>I was able to complete the entire course online.</li>
+            </ol>
+            <h3>What has been your proudest moment as a certified Infant/Pediatric Sleep Consultant?</h3>
+            <p>It’s difficult to name just one proud moment, because my Sleep Coaching journey has been so rewarding. It’d constantly filled with small celebrations! However, I’d have to say that one I am very proud of is how many families I’ve been able to work with in the last 8 months. In that time, I’ve helped over 50 infants and their families! I’ve also developed friendships, and connected with several women who follow me on social media.  </p>
+            <h3>What do you have in store for Pashionate About Sleep in 2020?</h3>
+            <p>I am going to continue to work hard and grow my business! I’ll see where my hard work and dedication takes me. If this past year has taught me anything, it’s that there are a LOT of surprises in store. Social media allows for people to connect in such unique ways, so hopefully more opportunities come my way!  </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <style jsx>{`
+      #guaranteeSection{background-image: url(${require('../images/bg-white-green-light.jpg')});background-size: cover;}
+      #partnerSection{background-image: url(${require('../images/bg-dark-green-navy.jpg')});background-size: cover;}
+    `}</style>
+
+  </DefaultLayout>
+);
+
+export default Page;
