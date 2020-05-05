cc4bdf58500cf749204941e0725c1a8dde29ecf4
Alex Monteiro <alex.monteiro@qccareerschool.com>
2020-05-05 14:01:28 -0400

    Updates student feature link on header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -50,6 +50,7 @@ export const Header: React.FC<Props> = ({ nav }) => {
                   </NavDropdown>
                   <NavDropdown title="About" id="aboutDropdown">
                     <Link href="/about"><a className="dropdown-item">About QC</a></Link>
+                    <Link href="/student-features/sarah-pashniak"><a className="dropdown-item">Student Feature</a></Link>
                     <Link href="/contact"><a className="dropdown-item">Contact</a></Link>
                   </NavDropdown>
                 </Nav>
