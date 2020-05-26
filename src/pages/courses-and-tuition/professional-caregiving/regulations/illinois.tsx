1f8116597e60bf7e72dacb93a6a7d9218c3e7c2c
Alex Monteiro <alex.monteiro@qccareerschool.com>
2020-05-26 14:58:35 -0400

    Creates illinois regulation page

--- src/pages/courses-and-tuition/professional-caregiving/regulations/illinois.tsx
+++ src/pages/courses-and-tuition/professional-caregiving/regulations/illinois.tsx
@@ -7,17 +7,17 @@ const Page: React.FC = () => {
   return (
     <DefaultLayout>
       <SEO
-        title="Professional Caregiver Regulations - By State - California"
-        description="Different states have different regulations for professional caregivers. Before you begin your training, make sure you understand these regulations!"
-        canonical="/courses-and-tuition/professional-caregiver/regulations/california"
+        title="Professional Caregiver Regulations - By State - Illinois"
+        description="Different states have different regulations for professional caregivers. Before you begin your training, make sure you understand the regulations in Illinois!"
+        canonical="/courses-and-tuition/professional-caregiver/regulations/illinois"
       />
 
       <section id="heroSection">
         <div className="container">
           <div className="row">
             <div className="col-12 col-sm-10 col-md-8 col-lg-7">
-              <h1>California</h1>
-              <h2 className="h5">In California, professional caregivers are referred to as “Home Care Aides” (HCAs).</h2>
+              <h1>Illinois</h1>
+              <h2 className="h5">In Illinois, Professional Caregivers fall under the category “Caregiver – Home Care – Home Service Worker (Non-Medical Home Care)” but are referred to as “Home Service Workers.”</h2>
             </div>
           </div>
         </div>
@@ -27,16 +27,15 @@ const Page: React.FC = () => {
         <div className="container">
           <div className="row">
             <div className="col-12 col-md-6 col-lg-5 offset-lg-1 mb-3 mb-md-0">
-              <p>HCAs in California are regulated by the state.</p>
+              <p>Home Service Workers employed by an agency are regulated by the state of Illinois.</p>
               <ul>
-                <li>You must have five hours of training in your field to become an HCA</li>
-                <li>HCAs must be registered with the Department of Social Services Home Care Registry</li>
-                <li>HCAs must also take on an additional five hours per year to maintain their registration</li>
+                <li>You must complete 8 hours of training to become a Home Service Worker in Illinois</li>
+                <li>Home Service Workers must also complete an additional 8 hours of training per year</li>            
               </ul>
             </div>
             <div className="col-12 col-md-6 col-lg-5">
               <p><img src={require('../../../../images/fc-regulations-icon-take-this-course.svg')} alt="Take This Course Icon" width="60" height="60" /></p>
-              <p>Take this course if: you would like to begin your career as a Caregiver, to receive higher pay in your current caregiving career, or to complete additional training.</p>
+              <p>Take this course if: you would like to begin your career as an independent Caregiver, to receive higher pay in your current caregiving career, or to complete additional training.</p>
               <p><a className="btn btn-secondary" href="https://enroll.qcwellnessstudies.com/?c[]=fc">ENROLL NOW</a></p>
             </div>
           </div>
@@ -47,11 +46,12 @@ const Page: React.FC = () => {
         <div className="container">
           <div className="col-12 col-lg-10 offset-lg-1 mb-6 mb-md-0">
             <h2>Funding</h2>
-            <p>Please note that, while these regulations are accurate at time of publication, laws can change quickly. It’s always a good idea to check on government websites or contact public health representatives to get the most up-to-date information.</p>
-            <p>If your client has served in the US Military and sustained a serious injury in the line of duty on or after September 11th, 2001, a Caregiver may be partially or fully reimbursed by <a className="text-info" href="https://www.benefits.gov/benefit/5918">Veteran’s’ Affairs</a>. Serious injuries include traumatic brain injury, psychological trauma, and other mental disorders.</p>
+            <p>The HCBS Waiver in Illinois allows eligible residents to receive care at home under strict requirements. If your client is between the ages of 60 and 64 and has a disability recognized by the Social Security Administration, the costs of caregiving may be partially or fully reimbursed by this reimbursement.</p>
+            <p>If your client has served in the US Military and sustained a serious injury in the line of duty on or after September 11th, 2001, a Caregiver may be partially or fully reimbursed by <a className="text-info" href="https://www.benefits.gov/benefit/5918">Veteran’s’ Affairs.</a></p>
             <p>Sources:</p>
-            <p><a className="text-info" href="https://www.ccld.dss.ca.gov/hcsregistry/About.aspx">https://www.ccld.dss.ca.gov/hcsregistry/About.aspx</a></p>
-            <p><a className="text-info" href="https://www.benefits.gov/benefit/5918">https://www.benefits.gov/benefit/5918</a></p>
+            <p><a className="text-info" href="http://www.ilga.gov/commission/jcar/admincode/077/077002450B00710R.html">http://www.ilga.gov/commission/jcar/admincode/077/077002450B00710R.html</a></p>
+            <p><a className="text-info" href="https://www.payingforseniorcare.com/illinois/medicaid-waivers/hcbs-waiver-for-the-elderly">https://www.payingforseniorcare.com/illinois/medicaid-waivers/hcbs-waiver-for-the-elderly</a></p>
+						<p><a className="text-info" href="https://www.benefits.gov/benefit/5918">https://www.benefits.gov/benefit/5918</a></p>
           </div>
         </div>
       </section>
