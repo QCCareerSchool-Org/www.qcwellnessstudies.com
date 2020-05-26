503cf6e287af6d592d8ead35219d5b2d2c4d5b29
Alex Monteiro <alex.monteiro@qccareerschool.com>
2020-05-26 15:09:13 -0400

    Creates pennsylvania regulation page

--- src/pages/courses-and-tuition/professional-caregiving/regulations/pennsylvania.tsx
+++ src/pages/courses-and-tuition/professional-caregiving/regulations/pennsylvania.tsx
@@ -7,17 +7,17 @@ const Page: React.FC = () => {
   return (
     <DefaultLayout>
       <SEO
-        title="Professional Caregiver Regulations - By State - California"
-        description="Different states have different regulations for professional caregivers. Before you begin your training, make sure you understand these regulations!"
-        canonical="/courses-and-tuition/professional-caregiver/regulations/california"
+        title="Professional Caregiver Regulations - By State - Pennsylvania"
+        description="Different states have different regulations for professional caregivers. Before you begin your training, make sure you understand the regulations in Pennsylvania!"
+        canonical="/courses-and-tuition/professional-caregiver/regulations/pennsylvania"
       />
 
       <section id="heroSection">
         <div className="container">
           <div className="row">
             <div className="col-12 col-sm-10 col-md-8 col-lg-7">
-              <h1>California</h1>
-              <h2 className="h5">In California, professional caregivers are referred to as “Home Care Aides” (HCAs).</h2>
+              <h1>Pennsylvania</h1>
+              <h2 className="h5">In Pennsylvania, Personal Caregivers are called “Direct Care Workers.”</h2>
             </div>
           </div>
         </div>
@@ -27,16 +27,14 @@ const Page: React.FC = () => {
         <div className="container">
           <div className="row">
             <div className="col-12 col-md-6 col-lg-5 offset-lg-1 mb-3 mb-md-0">
-              <p>HCAs in California are regulated by the state.</p>
+              <p>Direct Care Workers are regulated by the state of Pennsylvania, but there is no official training Caregivers must take. Instead, all Caregivers must pass a competency exam to practice in Pennsylvania.</p>
               <ul>
-                <li>You must have five hours of training in your field to become an HCA</li>
-                <li>HCAs must be registered with the Department of Social Services Home Care Registry</li>
-                <li>HCAs must also take on an additional five hours per year to maintain their registration</li>
+                <li>Licensing for Caregivers in Pennsylvania is competency-based.</li>              
               </ul>
             </div>
             <div className="col-12 col-md-6 col-lg-5">
               <p><img src={require('../../../../images/fc-regulations-icon-take-this-course.svg')} alt="Take This Course Icon" width="60" height="60" /></p>
-              <p>Take this course if: you would like to begin your career as a Caregiver, to receive higher pay in your current caregiving career, or to complete additional training.</p>
+              <p>Take this course if: you would like to prepare for the Pennsylvania competency exam, to receive higher pay in your current caregiving career, or to complete additional training.</p>
               <p><a className="btn btn-secondary" href="https://enroll.qcwellnessstudies.com/?c[]=fc">ENROLL NOW</a></p>
             </div>
           </div>
@@ -47,10 +45,10 @@ const Page: React.FC = () => {
         <div className="container">
           <div className="col-12 col-lg-10 offset-lg-1 mb-6 mb-md-0">
             <h2>Funding</h2>
-            <p>Please note that, while these regulations are accurate at time of publication, laws can change quickly. It’s always a good idea to check on government websites or contact public health representatives to get the most up-to-date information.</p>
+            <p>Medicaid programs to support caregiving are available to qualifying residents of Pennsylvania. However, to qualify for these programs, clients must require a nursing-home level of care. Your clients would probably not be reimbursed by Medicaid for your services.</p>
             <p>If your client has served in the US Military and sustained a serious injury in the line of duty on or after September 11th, 2001, a Caregiver may be partially or fully reimbursed by <a className="text-info" href="https://www.benefits.gov/benefit/5918">Veteran’s’ Affairs</a>. Serious injuries include traumatic brain injury, psychological trauma, and other mental disorders.</p>
             <p>Sources:</p>
-            <p><a className="text-info" href="https://www.ccld.dss.ca.gov/hcsregistry/About.aspx">https://www.ccld.dss.ca.gov/hcsregistry/About.aspx</a></p>
+            <p><a className="text-info" href="https://www.health.pa.gov/topics/Documents/Facilities%20and%20Licensing/Chapter611.pdf">https://www.health.pa.gov/topics/Documents/Facilities%20and%20Licensing/Chapter611.pdf</a></p>
             <p><a className="text-info" href="https://www.benefits.gov/benefit/5918">https://www.benefits.gov/benefit/5918</a></p>
           </div>
         </div>
