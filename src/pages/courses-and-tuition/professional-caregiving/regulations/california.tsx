3156e18dcefbb91e40b3f1e45cc29bf4c6650f00
Alex Monteiro <alex.monteiro@qccareerschool.com>
2020-05-27 10:06:50 -0400

    Updates california page content

--- src/pages/courses-and-tuition/professional-caregiving/regulations/california.tsx
+++ src/pages/courses-and-tuition/professional-caregiving/regulations/california.tsx
@@ -35,8 +35,13 @@ const Page: React.FC = () => {
               </ul>
             </div>
             <div className="col-12 col-md-6 col-lg-5">
-              <p><img src={require('../../../../images/fc-regulations-icon-take-this-course.svg')} alt="Take This Course Icon" width="60" height="60" /></p>
-              <p>Take this course if: you would like to begin your career as a Caregiver, to receive higher pay in your current caregiving career, or to complete additional training.</p>
+              <p className="text-center"><img src={require('../../../../images/fc-regulations-icon-take-this-course.svg')} alt="Take This Course Icon" width="60" height="60" /></p>
+              <h2 className="h5 text-center">Take this course if:</h2>
+              <ul>
+                <li>You would like to begin your career as a Caregiver</li>
+                <li>To receive higher pay in your current caregiving career</li>
+                <li>To complete additional training</li>             
+              </ul>                    
               <p><a className="btn btn-secondary" href="https://enroll.qcwellnessstudies.com/?c[]=fc">ENROLL NOW</a></p>
             </div>
           </div>
@@ -46,9 +51,10 @@ const Page: React.FC = () => {
       <section className="bg-light text-dark">
         <div className="container">
           <div className="col-12 col-lg-10 offset-lg-1 mb-6 mb-md-0">
-            <h2>Funding</h2>
-            <p>Please note that, while these regulations are accurate at time of publication, laws can change quickly. It’s always a good idea to check on government websites or contact public health representatives to get the most up-to-date information.</p>
+            <h2>Funding</h2>  
+            <p>Multiple Medicaid programs are available to residents of California. However, these programs are usually limited to those people who require a nursing-home level of care. Generally speaking, your clients will not be reimbursed by Medicaid for your services.</p>          
             <p>If your client has served in the US Military and sustained a serious injury in the line of duty on or after September 11th, 2001, a Caregiver may be partially or fully reimbursed by <a className="text-info" href="https://www.benefits.gov/benefit/5918">Veteran’s’ Affairs.</a> Serious injuries include traumatic brain injury, psychological trauma, and other mental disorders.</p>
+            <p className="font-italic">Please note that while these regulations are accurate at time of publication, laws can change quickly. It’s always a good idea to check on government websites or contact public health representatives to get the most up-to-date information.</p>
             <p>Sources:</p>
             <p><a className="text-info" href="https://www.ccld.dss.ca.gov/hcsregistry/About.aspx">https://www.ccld.dss.ca.gov/hcsregistry/About.aspx</a></p>
             <p><a className="text-info" href="https://www.benefits.gov/benefit/5918">https://www.benefits.gov/benefit/5918</a></p>
