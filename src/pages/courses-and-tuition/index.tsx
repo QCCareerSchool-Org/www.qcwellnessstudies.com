e984d19b6f060bba94f735d72dd83f80f99612b7
Alex Monteiro <alex.monteiro@qccareerschool.com>
2020-06-28 15:35:22 -0400

    Updates courses and tuition page

--- src/pages/courses-and-tuition/index.tsx
+++ src/pages/courses-and-tuition/index.tsx
@@ -20,13 +20,14 @@ const Page: React.FC = () => (
         <hr className="mt-5" />
         <div className="row">
           <div className="col-12 col-sm-8 offset-sm-2 col-lg-5 offset-lg-0 col-xl-4">
-            <PriceCard courses={[ 'fc' ]} />
+            <PriceCard courses={[ 'ic' ]} />
           </div>
           <div className="col-12 mb-5 col-md-12 col-lg-7 mb-lg-0 col-xl-8 text-center text-lg-left order-first order-lg-last">
             <h2 className="text-dark">Caregiver</h2>
-            <p>Professional caregivers are in extremely high demand and the industry is expected to grow very quickly over the next few years! As a caregiver, you’ll help your clients maintain their independence by providing companionship and helping them with daily tasks they may find difficult to do on their own.</p>
-            <p>In the professional course, you’ll learn the soft and practical skills you need to be a professional caregiver. You'll also learn how to run your own caregiving business so that you can bill clients directly instead of working through an agency. This is a great way to earn more income!</p>
-            <Link href="/courses-and-tuition/professional-caregiving"><a className="btn btn-secondary">LEARN MORE</a></Link>
+            <p>As a caregiver, you’ll help vulnerable individuals maintain their independence by providing companionship and helping them with daily tasks that they may find difficult to do on their own. </p>
+            <p><strong>Professional caregivers</strong> are in extremely high demand. The industry is expected to grow very quickly over the next few years! QC’s Professional Caregiver course will give you the skills to work in a professional environment, either working for an agency or independently by running your own caregiver business.</p>
+            <p>If you want to be a <strong>personal caregiver</strong> for a loved one, the personal caregiver course will give you the skills you need to do the job safely.</p>
+            <Link href="/caregiver"><a className="btn btn-secondary">LEARN MORE</a></Link>
           </div>
         </div>
       </div>
