c40951e3d70bbeed060d9f4fa917619ecaa760f5
Alex Monteiro <alex.monteiro@qccareerschool.com>
2020-07-03 15:46:58 -0400

    Updates offsets

--- src/pages/caregiver.tsx
+++ src/pages/caregiver.tsx
@@ -19,13 +19,13 @@ const Page: React.FC = () => (
         <h2 className="text-dark text-center h3 mb-5">QC has 2 courses to meet your needs</h2>
         <hr />
         <div className="row d-flex align-items-center">
-          <div className="col-12 mb-4 col-sm-10 col-md-8 offset-md-2 offset-lg-0 col-lg-6 mb-lg-0 text-center">
+          <div className="col-12 mb-4 col-sm-10 col-md-8 offset-sm-1 offset-md-2 offset-lg-0 col-lg-6 mb-lg-0 text-center">
             <p className="lead">PROFESSIONAL CAREGIVER</p>
             <p>Join one of the world’s fastest growing industries! Work for established caregiving agencies or freelance as an independent caregiver.</p>
             <p>This course includes a professional certification!</p>
             <Link href="/courses-and-tuition/professional-caregiving"><a className="btn btn-secondary mb-4">LEARN MORE</a></Link>
           </div>
-          <div className="col-12 col-sm-10 col-md-8 offset-md-2 offset-lg-0 col-lg-6 text-center">
+          <div className="col-12 col-sm-10 col-md-8 offset-sm-1 offset-md-2 offset-lg-0 col-lg-6 text-center">
             <p className="lead">PERSONAL CAREGIVER</p>
             <p>Gain the skills you need to safely care for your loved ones at home.</p>
             <p>This course includes a certificate of completion but not a professional certification.</p>
