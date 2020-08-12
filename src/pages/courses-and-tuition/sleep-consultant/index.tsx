80cfebc8990091936a18b05043aaff6ae7208a83
Alex Monteiro <alex.monteiro@qccareerschool.com>
2020-08-11 21:25:01 -0400

    Updates promo section and date

--- src/pages/courses-and-tuition/sleep-consultant/index.tsx
+++ src/pages/courses-and-tuition/sleep-consultant/index.tsx
@@ -299,17 +299,11 @@ const Page: React.FC = () => {
       </section>
 
       <section>
-        <div className="container text-light align-items-center">
-          <div className="row d-flex align-items-center">      
-            <div className="col-12 text-center">
-            <hr />
-              <h2 className="text-dark text-center">LIMITED TIME OFFER</h2>        
-              <p><img className="img-fluid mw-50" src={require('../../../images/promo-save-100.svg')} alt="Promo Save 100" /></p>
-              <h2 className="text-dark text-center h3 mb-5">Until August 17th, save an extra $100 on your sleep consultant course tuition. This discount will be automatically applied when you enroll.</h2>
-              <p className="text-center"><a className="btn btn-secondary btn-lg" href="https://enroll.qcwellnessstudies.com/?c[]=sl">ENROLL NOW</a></p>
-              <hr />
-            </div>
-          </div>    
+        <div className="container text-dark text-center">
+           <h2>LIMITED TIME OFFER</h2>
+           <p><img className="img-fluid" src={require('../../../images/promo-save-100.svg')} alt="Promo Save 100" /></p>
+           <p className="lead mb-5">Until August 19th, save an extra $100 on your sleep consultant course tuition. This discount will be automatically applied when you enroll.</p>
+           <p><a className="btn btn-secondary btn-lg" href="https://enroll.qcwellnessstudies.com/?c[]=sl">ENROLL NOW</a></p>
         </div>
       </section>
 
