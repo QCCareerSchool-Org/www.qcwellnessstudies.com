1ed265d54b864dc39c11e0d1f19affbecb083aa8
Alex Monteiro <AlexMonteiro@Rome.qc.local>
2020-10-30 12:52:47 -0400

    Updates black friday promos

--- src/pages/courses-and-tuition/professional-caregiving/index.tsx
+++ src/pages/courses-and-tuition/professional-caregiving/index.tsx
@@ -347,8 +347,8 @@ const Page: React.FC = () => {
         <div className="row">
           <div className="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2">
             <div className="container text-dark text-center">
-              <p><img className="img-fluid" src={require('../../../images/promo-inlay-october.png')} alt="Promo Save $100" /></p>
-              <p className="lead mb-5">Until October 30th, save an extra $100 on your professional caregiver course tuition.<br />This discount will be automatically applied when you enroll.</p>
+              <p><img className="img-fluid" src={require('../../../images/promo-inlay-october.png')} alt="Promo Save $200" /></p>
+              <p className="lead mb-5">This Black Friday, save an extra $200 on your professional caregiver course tuition.<br />This discount will be automatically applied when you enroll.</p>
               <p><a className="btn btn-secondary btn-lg" href="https://enroll.qcwellnessstudies.com/?c[]=fc">ENROLL NOW</a></p>
             </div>
           </div>
