23dc42f17f212859372da82e163d4028d5699170
Alex <AlexMonteiro@Rome.qc.local>
2021-02-23 08:56:50 -0500

    Updates image sizes in student resources

--- src/pages/student-resources/blog/7-tips-for-travelling-with-a-baby.tsx
+++ src/pages/student-resources/blog/7-tips-for-travelling-with-a-baby.tsx
@@ -10,7 +10,7 @@ const Page: NextPage = () => (
 
         <SEO
             title="7 Tips for Travelling with a Baby"
-            description=""
+            description="Following an infant’s sleep plan while travelling can be a challenge. A new sleep environment, a change in routine or different eating habits can all impact an infant’s sleep. Consider these seven tips for travelling with an infant, without disrupting the sleep schedule."
             canonical="/student-resources/blog/7-tips-for-travelling-with-a-baby"
         />
 
@@ -26,32 +26,38 @@ const Page: NextPage = () => (
 
         <section>
             <div className="container col-12 mb-4 col-sm-10 col-md-8 col-lg-6 mb-lg-0">
-                <div className="row mb-4">
+                <div className="row mb-4">        
                     <p className="lead">Following an infant’s sleep plan while travelling can be a challenge. A new sleep environment, a change in routine or different eating habits can all impact an infant’s sleep. Consider these seven tips for travelling with an infant, without disrupting the sleep schedule.</p>
-                    <h2>1. Plan Accomodation in Advance.</h2>
-                    <p>Planning is key to a successful trip. When travelling with children, accommodations should include a designated space for babies and young children to sleep. Check that accommodations will include a separate living space for children and will be equipped with a crib or child’s bed.  </p>
-                    <div>
-                        <p><img className="img-fluid" src={require('../../../images/sl-baby-sleeping-in-crib.jpg')} alt="Baby sleeping in a crib" /></p>
-                    </div>
+                    <h4>1. Plan Accomodation in Advance.</h4>
+                    <p>Planning is key to a successful trip. When travelling with children, accommodations should include a designated space for babies and young children to sleep. Check that accommodations will include a separate living space for children and will be equipped with a crib or child’s bed.</p>
+                
                     <h4>2. Bring comforts from home.</h4>
                     <p>Bring comforting items from home to help soothe and comfort infants in unfamiliar settings. These home comforts could include:</p>
                     <ul>
                         <li>Favorite sleepwear</li>
-                        <li>A favorite stuffed toy, such as a teddy bear </li>
+                        <li>A favorite stuffed toy, such as a teddy bear</li>
                         <li>A nightlight</li>
                     </ul>
-
-                    <h2>3.	Stay in your time zone.</h2>
-                    <p>Planning is key to a successful trip. When travelling with children, accommodations should include a designated space for babies and young children to sleep. Check that accommodations will include a separate living space for children and will be equipped with a crib or child’s bed.</p>                 
+                    <div className="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-10 offset-lg-1 text-center flex-column"> 
+                        <p><img className="img-fluid" src={require('../../../images/sl-sweet-newborn-baby-sleeps-toy-hare.jpg')} alt="newborn baby sleeps with toy" /></p>
+                    </div>               
                     <h4>3.	Stay in your time zone.</h4>
                     <p>If possible, travel within your home time zone. Altering a sleep schedule to suit a new time zone is confusing for anyone but infants and children will find adjusting to a new schedule especially difficult. If you do travel outside your time zone, try to change the sleep plan gradually to suit to new schedule. You might even start adjusting the baby’s schedule before the trip to give infants time to adjust.</p>
                     <h4>4.	Maintain regular nap times.</h4>
                     <p>Follow through with nap times as much as possible while travelling to keep the baby in a familiar routine. Naps will help prevent the infant from becoming grumpy and unhappy during the trip.</p>
                     <h4>5.	Follow the usual bedtime routine as closely as possible</h4>
                     <p>Routine helps an infant settle and know that bedtime is coming soon. Follow through with the usual bedtime activities, such as having a bath, reading a story, etc., to establish routine while away from home.</p>
-                    <h4>6.	Relax the routine for events and activities.</h4>
+                    <div className="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-10 offset-lg-1 text-center flex-column">
+                        <p><img className="img-fluid" src={require('../../../images/sl-asian-young-pretty-mother-giving-bottle.jpg')} alt="mother giving baby bottle" /></p>
+                    </div>                    
+                    <h4>6.	Relax the routine for events and activities.</h4>                    
                     <p>Be flexible and ease the schedule slightly to suit travel plans and events, such as weddings, parties, day trips and family celebrations. Allow the infant to fall asleep outside of the regular nap times, such as in a car or plane, to keep the baby in a good mood later on. Trying to follow a rigid schedule will cause stress for the parents and infant!</p>
-                </div>
+                    <h4>7.	Reinstate the sleep plan at home.</h4>                    
+                    <p>After the trip, get back to the regular sleep plan and routine as quickly as possible. Put down the infant at the normal nap times and follow the usual bedtime routine. However, if the infant is adjusting between time zones, change the sleep schedule gradually. The baby will need time to adapt.</p>
+                    <div className="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-10 offset-lg-1 text-center flex-column">                    
+                     <p><img className="img-fluid" src={require('../../../images/sl-adorable-baby-drinking-milk-blue-bassinet.jpg')} alt="baby sleeping in a crib" /></p>
+                     </div>
+                </div>                
             </div>
         </section>
 
