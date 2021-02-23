6cbfa8ead01a20cb301a2b3ad117be524154e0a6
Alex <AlexMonteiro@Rome.qc.local>
2021-02-23 09:22:56 -0500

    Updates tipsheet layout

--- src/pages/student-resources/tipsheets/7-tips-for-travelling-with-a-baby.tsx
+++ src/pages/student-resources/tipsheets/7-tips-for-travelling-with-a-baby.tsx
@@ -11,12 +11,12 @@ const Page: NextPage = () => (
 
         <SEO
             title="7 Tips for Travelling with a Baby"
-            description=""
-            canonical="/student-resources/tipsheet/7-tips-for-travelling-with-a-baby"
+            description="Following an infant’s sleep plan while travelling can be a challenge. A new sleep environment, a change in routine or different eating habits can all impact an infant’s sleep. Consider the tips in this tipsheet for travelling with an infant, without disrupting the sleep schedule"
+            canonical="/student-resources/tipsheets/7-tips-for-travelling-with-a-baby"
         />
 
         <section id="heroSection">
-            <div className="container col-12  mb-4 col-sm-10 col-md-8 col-lg-6 mb-lg-0">
+            <div className="container col-12 mb-4 col-sm-10 col-md-8 col-lg-6 mb-lg-0">
                 <div className="row d-flex align-items-center">
                     <div className="text-light text-center">
                         <h1>7 Tips for Travelling with a Baby</h1>
@@ -27,20 +27,37 @@ const Page: NextPage = () => (
 
         <section>
             <div className="container col-12 mb-4 col-sm-10 col-md-8 col-lg-6 mb-lg-0">
-                <div className="row">
-                    <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    <h2>Plan Accomodation in Advance.</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <div>
-                        <p><img className="img-fluid" src={require('../../../images/sl-baby-sleeping-in-crib.jpg')} alt="Baby sleeping in a crib" /></p>
+                <div className="row mb-4">
+                    <p className="lead">Following an infant’s sleep plan while travelling can be a challenge. A new sleep environment, a change in routine or different eating habits can all impact an infant’s sleep. Consider these seven tips for travelling with an infant, without disrupting the sleep schedule.</p>
+                    <h4>1. Plan Accomodation in Advance.</h4>
+                    <p>Planning is key to a successful trip. When travelling with children, accommodations should include a designated space for babies and young children to sleep. Check that accommodations will include a separate living space for children and will be equipped with a crib or child’s bed.</p>
+                    <h4>2. Bring comforts from home.</h4>
+                    <p>Bring comforting items from home to help soothe and comfort infants in unfamiliar settings. These home comforts could include:</p>
+                    <ul>
+                        <li>Favorite sleepwear</li>
+                        <li>A favorite stuffed toy, such as a teddy bear</li>
+                        <li>A nightlight</li>
+                    </ul>
+                    <div className="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-10 offset-lg-1 text-center flex-column">
+                        <p><img className="img-fluid" src={require('../../../images/sl-sweet-newborn-baby-sleeps-toy-hare.jpg')} alt="newborn baby sleeps with toy" /></p>
                     </div>
-                    <h4>Plan Accomodation in Advance.</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <p className="lead">Download a free tip sheet on 7 Tips for Travelling with a Baby</p>
-                </div>
-                <div className="row">
+                    <h4>3.	Stay in your time zone.</h4>
+                    <p>If possible, travel within your home time zone. Altering a sleep schedule to suit a new time zone is confusing for anyone but infants and children will find adjusting to a new schedule especially difficult. If you do travel outside your time zone, try to change the sleep plan gradually to suit to new schedule. You might even start adjusting the baby’s schedule before the trip to give infants time to adjust.</p>
+                    <h4>4.	Maintain regular nap times.</h4>
+                    <p>Follow through with nap times as much as possible while travelling to keep the baby in a familiar routine. Naps will help prevent the infant from becoming grumpy and unhappy during the trip.</p>
+                    <h4>5.	Follow the usual bedtime routine as closely as possible</h4>
+                    <p>Routine helps an infant settle and know that bedtime is coming soon. Follow through with the usual bedtime activities, such as having a bath, reading a story, etc., to establish routine while away from home.</p>
+                    <div className="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-10 offset-lg-1 text-center flex-column">
+                        <p><img className="img-fluid" src={require('../../../images/sl-asian-young-pretty-mother-giving-bottle.jpg')} alt="mother giving baby bottle" /></p>
+                    </div>
+                    <h4>6.	Relax the routine for events and activities.</h4>
+                    <p>Be flexible and ease the schedule slightly to suit travel plans and events, such as weddings, parties, day trips and family celebrations. Allow the infant to fall asleep outside of the regular nap times, such as in a car or plane, to keep the baby in a good mood later on. Trying to follow a rigid schedule will cause stress for the parents and infant!</p>
+                    <h4>7.	Reinstate the sleep plan at home.</h4>
+                    <p>After the trip, get back to the regular sleep plan and routine as quickly as possible. Put down the infant at the normal nap times and follow the usual bedtime routine. However, if the infant is adjusting between time zones, change the sleep schedule gradually. The baby will need time to adapt.</p>
                     <p><Link href="/tip-sheets/7-tips-travelling-baby.pdf"><a className="btn btn-primary"><strong>Download Tipsheet</strong></a></Link></p>
+                    <div className="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-10 offset-lg-1 text-center flex-column">
+                        <p><img className="img-fluid" src={require('../../../images/sl-adorable-baby-drinking-milk-blue-bassinet.jpg')} alt="baby sleeping in a crib" /></p>
+                    </div>
                 </div>
             </div>
         </section>
