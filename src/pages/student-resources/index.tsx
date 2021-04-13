27afe8d0fdc7b37d56fd2192fbea9655a31d44bf
Alex <AlexMonteiro@Rome.qc.local>
2021-04-13 14:47:04 -0400

    Updates article link in index page

--- src/pages/student-resources/index.tsx
+++ src/pages/student-resources/index.tsx
@@ -75,6 +75,7 @@ const Page: NextPage = () => (
             <p className="lead"><Link href="/student-resources/blog/working-with-helicopter-parents"><a><strong>Working with Helicopter Parents</strong></a></Link></p>
             <p className="lead"><Link href="/student-resources/blog/6-common-training-methods-of-an-infant-sleep-consultant"><a><strong>The 6 Common Training Methods of an Infant Sleep Consultant</strong></a></Link></p>
             <p className="lead"><Link href="/student-resources/blog/baby-sign-language-for-beginners"><a><strong>Baby Sign Language for Beginners</strong></a></Link></p>
+            <p className="lead"><Link href="/student-resources/blog/working-during-covid"><a><strong>Learning &amp; Working as a Sleep Consultant During COVID-19</strong></a></Link></p>
           </div>
         </div>
       </div>
