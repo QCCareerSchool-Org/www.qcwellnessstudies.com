7854dc158257099165aa3ed929b8c33b697bc645
Dave Welsh <dave@qccareerschool.com>
2022-02-07 13:10:55 -0500

    Remove sentence about materials from about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -46,7 +46,7 @@ const Page: NextPage = () => (
         <div className="col-12 col-lg-8 offset-lg-2 mb-6 mb-md-0">
           <p><img src={require('../images/21-day-guarentee.svg')} alt="21-day-guarentee" /></p>
           <h2 className="text-dark">QC&apos;s Guarantee</h2>
-          <p>At QC, providing high quality online wellness courses is our top priority. With QC&apos;s 100% money-back guarantee, you&apos;ll have 21 days to review your course materials from the day you receive them. If you have a question or concern, contact us. If, in the 21 days, you decide the course isn&apos;t for you, we&apos;ll give you a full refund. Simply return the materials (unused, with a tracking number) and your tuition will be immediately refunded.</p>
+          <p>At QC, providing high quality online wellness courses is our top priority. With QC&apos;s 100% money-back guarantee, you&apos;ll have 21 days to review your course materials from the day you receive them. If you have a question or concern, contact us. If, in the 21 days, you decide the course isn&apos;t for you, we&apos;ll give you a full refund.</p>
         </div>
       </div>
     </section>
