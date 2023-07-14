9f82d7dc0965cd311b768b8e3e4f9653c6d2d02c
Dave Welsh <dave@qccareerschool.com>
2023-07-14 10:38:03 -0400

    Added Google Ads sale conversion tracking

--- src/lib/ga.ts
+++ src/lib/ga.ts
@@ -37,4 +37,10 @@ export const gaSale = (enrollment: Enrollment): void => {
       quantity: 1,
     })),
   });
+
+  gaEvent('conversion', {
+    send_to: 'AW-1071836607/rla_CMLg3ZgBEL_bi_8D', // eslint-disable-line camelcase
+    value: enrollment.cost,
+    currency: enrollment.currencyCode,
+  });
 };
