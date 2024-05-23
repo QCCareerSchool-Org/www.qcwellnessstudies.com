6bc243b59b9f05e07c8341cf2a7449f3bcf0b24f
Dave Welsh <dave@qccareerschool.com>
2024-05-23 14:23:27 -0400

    Switched to .page.tsx extension

--- __tests__/pages/index.tsx
+++ __tests__/pages/index.tsx
@@ -6,7 +6,7 @@ import { render } from '@testing-library/react';
 import { axe, toHaveNoViolations } from 'jest-axe';
 import React from 'react';
 
-import Page from '../../src/pages/index';
+import Page from '../../src/pages/index.page';
 
 expect.extend(toHaveNoViolations);
 
