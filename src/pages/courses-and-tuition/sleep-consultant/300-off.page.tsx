76053a06d92b165efa95aed81fb405f06666c879
Dave Welsh <dave@qccareerschool.com>
2024-08-16 14:52:00 -0400

    Added 300-off page

---
+++ src/pages/courses-and-tuition/sleep-consultant/300-off.page.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { DeadlineFunnelScript } from '../../../components/DeadlineFunnelScript';
+import { LandingPageLayout } from '../../../layouts/LandingPageLayout';
+import { NextPageWithLayout } from '../../_app.page';
+import OriginalPage from './index.page';
+
+const Page: NextPageWithLayout = () => (
+  <>
+    <DeadlineFunnelScript />
+    <OriginalPage enrollPath="https://enroll.qcwellnessstudies.com/150-off" />
+  </>
+);
+
+Page.getLayout = function Layout(page) {
+  return <LandingPageLayout>{page}</LandingPageLayout>;
+};
+
+export default Page;
