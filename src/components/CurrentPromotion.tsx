5520d17befdcb11e31d078911ff95a3ed3ca7b20
Maryam Hassan <maryam.hassan@qccareerschool.com>
2025-12-18 18:17:48 -0500

    Update date range to Dec 26 promo

--- src/components/CurrentPromotion.tsx
+++ src/components/CurrentPromotion.tsx
@@ -10,7 +10,7 @@ interface Props {
 }
 
 export const CurrentPromotion: FC<Props> = ({ date, countryCode, newYearsDiscountAmount, newYearsEnrollHref }) => {
-  if (date >= Date.UTC(2025, 11, 16, 8) && date < Date.UTC(2026, 0, 3, 8)) { // 2025-12-26T03:00 (8:00 UTC) to 2026-01-03T03:00 (8:00 UTC)
+  if (date >= Date.UTC(2025, 11, 26, 8) && date < Date.UTC(2026, 0, 3, 8)) { // 2025-12-26T03:00 (8:00 UTC) to 2026-01-03T03:00 (8:00 UTC)
     return (
       <NewYears2025
         countryCode={countryCode}
