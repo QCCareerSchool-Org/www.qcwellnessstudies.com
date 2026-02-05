aaf5fd250da57c4e4c53864de92cff48a19c8611
Dave Welsh <dave@qccareerschool.com>
2026-02-05 16:00:25 -0500

    Allow route filename

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  pageExtensions: [ 'page.tsx', 'page.ts', 'page.jsx', 'page.js' ],
+  pageExtensions: [ 'page.tsx', 'page.ts', 'page.jsx', 'page.js', 'route.ts', 'route.tsx', 'route.js', 'route.jsx' ],
   sassOptions: {
     quietDeps: true, // hide deprecation warnings from node_modules
     // If your sass-loader / Sass version supports it, you can also do:
