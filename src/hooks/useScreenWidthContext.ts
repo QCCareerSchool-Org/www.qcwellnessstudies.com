a8cd22a9592fe65e64a72e57c7858925f9c62602
Dave Welsh <dave@qccareerschool.com>
2026-04-14 14:34:48 -0400

    Fixed providers

--- src/hooks/useScreenWidthContext.ts
+++ src/hooks/useScreenWidthContext.ts
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 
 import { ScreenWidthContext } from '@/providers/ScreenWidthProvider';
 
-export const useScreenWidthContext = (): number => {
+export const useScreenWidthContext = (): number | null => {
   const context = useContext(ScreenWidthContext);
   if (context === undefined) {
     throw new Error('useScreenWidthContext must be used within a ScreenWidthProvider');
--- src/providers/ScreenWidthProvider.tsx
+++ src/providers/ScreenWidthProvider.tsx
@@ -2,14 +2,14 @@ import type { FC, ReactNode } from 'react';
 import { createContext } from 'react';
 import { useWindowListener } from 'use-window-listener';
 
-export const ScreenWidthContext = createContext<number | undefined>(undefined);
+export const ScreenWidthContext = createContext<number | null | undefined>(undefined);
 
 interface Props {
   children: ReactNode;
 }
 
 export const ScreenWidthProvider: FC<Props> = ({ children }) => {
-  const state = useWindowListener('resize', w => w.innerWidth);
+  const state = useWindowListener('resize', w => w.innerWidth, null);
 
   return (
     <ScreenWidthContext.Provider value={state}>
